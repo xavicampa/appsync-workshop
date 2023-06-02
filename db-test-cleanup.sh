1d9c1d6a20687dc28b62c9692afd6174b8aac136
Anders Bjørnestad <anders.bjornestad@webstep.no>
2023-06-02 14:42:56 +0200

    update with ID

--- db-test-cleanup.sh
+++ db-test-cleanup.sh
@@ -1,12 +1,12 @@
 #/bin/bash
-
+echo Enter ID to delete:
+read DELETEID
 
 echo "Deleting secrets..."
-aws secretsmanager delete-secret --secret-id /workshop/hotelinventory-db-manager-$1 --query Name --output text
-aws secretsmanager delete-secret --secret-id /workshop/hotelinventory-db-roomview-$1 --query Name --output text
+aws secretsmanager delete-secret --secret-id /workshop/hotelinventory-db-manager-$DELETEID --query Name --output text
+aws secretsmanager delete-secret --secret-id /workshop/hotelinventory-db-roomview-$DELETEID --query Name --output text
 echo "Secrets deleted"
 
 echo "Deleting database..."
-aws rds delete-db-cluster --db-cluster-identifier hotelinventory-$1 --skip-final-snapshot --query DBCluster
-aws rds wait db-cluster-deleted --db-cluster-identifier hotelinventory-$1
-echo "Database deleted"
+aws rds delete-db-cluster --db-cluster-identifier hotelinventory-$DELETEID --skip-final-snapshot --query DBCluster.DBClusterIdentifier --output text
+
